3d42e71f7bafef26047acef58d142f50c0fb5190
Charles Ouimet <charles.ouimet@gmail.com>
2025-01-30 14:04:14 -0500

    Simplify `RealEstate` contract to have one less dependency
    Used https://github.com/OpenZeppelin/openzeppelin-contracts/blob/840c974028316f3c8172c1b8e5ed67ad95e255ca/contracts/mocks/docs/token/ERC721/GameItem.sol#L5 as a reference for extending a `ERC721URIStorage` contract.

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,19 +1,22 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.28;
-import "@openzeppelin/contracts/utils/Counters.sol";
+
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract RealEstate is ERC721URIStorage {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+    uint256 private _nextTokenId;
 
     constructor() ERC721("Real Estate", "REAL") {}
 
-        function mint(string memory tokenURI) public returns (uint256) {
-        _tokenIds.increment();
+    function mint(string memory tokenURI) public returns (uint256) {
+        // We use the `++` operator _before_ the assignment to ensure the local variable is incremented.
+        // While technically the ERC721 standard doesn't mandate starting from 0 or 1, 
+        // starting from 1 has become the de facto standard.
+        // This trick is used to increment the _nextTokenId before using it and
+        // ensures the totalSupply is always correct.
+        uint256 newItemId = ++_nextTokenId;
 
-        uint256 newItemId = _tokenIds.current();
         _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
 
@@ -21,6 +24,6 @@ contract RealEstate is ERC721URIStorage {
     }
 
     function totalSupply() public view returns (uint256) {
-        return _tokenIds.current();
+        return _nextTokenId;
     }
 }
