323315173360c0cfa1bb8855bfa387c7c9760e7d
Charles Ouimet <charles.ouimet@gmail.com>
2025-02-18 08:09:03 -0500

    Converted unit tests to use Hardhat's `loadFixture()` instead of `beforeEach()` blocks.
    There are no functional changes to the tests. The only changes are that tests are now explicitly calling `loadFixture()` with a fixture that suits their requirements and a few calls to `<something>.address` were replaced with `await <something>.getAddress()`.
    
    Ref: https://hardhat.org/tutorial/testing-contracts#reusing-common-test-setups-with-fixtures

--- test/Escrow.js
+++ test/Escrow.js
@@ -1,61 +1,114 @@
+const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers")
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
 const tokens = (n) => {
-    return ethers.utils.parseUnits(n.toString(), 'ether')
+    return ethers.parseUnits(n.toString(), 'ether')
+}
+
+async function deployRealEstateFixture() {
+  // Setup accounts
+  const [buyer, seller, inspector, lender] = await ethers.getSigners()
+
+  // Deploy Real Estate
+  const RealEstate = await ethers.getContractFactory('RealEstate')
+  const realEstate = await RealEstate.deploy()
+
+  return { realEstate, buyer, seller, inspector, lender }
+}
+
+async function mintPropertyFixture() {
+  const { realEstate, buyer, seller, inspector, lender } = await loadFixture(deployRealEstateFixture)
+
+  // Mint
+  let transaction = await realEstate.connect(seller).mint("https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS")
+  await transaction.wait()
+ 
+  return { buyer, seller, inspector, lender, realEstate };
+}
+
+async function deployEscrowFixture() {
+  const { buyer, seller, inspector, lender, realEstate } = await loadFixture(mintPropertyFixture);
+
+  // Deploy Escrow
+  const Escrow = await ethers.getContractFactory('Escrow')
+
+  const escrow = await Escrow.deploy(
+    await realEstate.getAddress(),
+    seller.address,
+    inspector.address,
+    lender.address
+  )
+  
+  return { buyer, seller, inspector, lender, realEstate, escrow }
+}
+
+async function listPropertyFixture() {
+  const { buyer, seller, inspector, lender, realEstate, escrow } = await loadFixture(deployEscrowFixture);
+
+  // Approve Property
+  let transaction = await realEstate.connect(seller).approve(await escrow.getAddress(), 1)
+  await transaction.wait()
+
+  // List property
+  transaction = await escrow.connect(seller).list(1, buyer.address, tokens(10), tokens(5));
+  await transaction.wait()
+
+  return { buyer, seller, inspector, lender, realEstate, escrow }
+}
+
+async function finalizeSaleFixture() { 
+  const { buyer, seller, inspector, lender, realEstate, escrow } = await loadFixture(listPropertyFixture);
+
+  let transaction = await escrow.connect(buyer).depositEarnest(1, { value: tokens(5) });
+  await transaction.wait();
+
+  transaction = await escrow.connect(inspector).updateInspectionStatus(1, true);
+  await transaction.wait();
+
+  transaction = await escrow.connect(buyer).approveSale(1);
+  await transaction.wait();
+
+  transaction = await escrow.connect(seller).approveSale(1);
+  await transaction.wait();
+
+  transaction = await escrow.connect(lender).approveSale(1);
+  await transaction.wait();
+
+  await lender.sendTransaction({ to: await escrow.getAddress(), value: tokens(5) });    
+  
+  transaction = await escrow.connect(lender).finalizeSale(1);
+  await transaction.wait();
+
+  return { buyer, seller, inspector, lender, realEstate, escrow }
 }
 
 describe('Escrow', () => {
-  let buyer, seller, inspector, lender;
-  let realEstate, escrow;
-
-  beforeEach(async () => {
-    // Setup accounts
-    [buyer, seller, inspector, lender] = await ethers.getSigners()
-
-    // Deploy Real Estate
-    const RealEstate = await ethers.getContractFactory('RealEstate')
-    realEstate = await RealEstate.deploy()
-
-    // Mint 
-    let transaction = await realEstate.connect(seller).mint("https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS")
-    await transaction.wait()
-
-    // Deploy Escrow
-    const Escrow = await ethers.getContractFactory('Escrow')
-    escrow = await Escrow.deploy(
-      realEstate.address,
-      seller.address,
-      inspector.address,
-      lender.address
-    )
-    
-    // Approve Property
-    transaction = await realEstate.connect(seller).approve(escrow.address, 1)
-    await transaction.wait()
-  
-    // List property
-    transaction = await escrow.connect(seller).list(1, buyer.address, tokens(10), tokens(5));
-    await transaction.wait()
-  })
-
-describe('Deployment', () => {
+  describe('Deployment', () => {
     it('Returns NFT address', async () => {
+      const { realEstate, escrow } = await loadFixture(deployEscrowFixture)
+
       const result = await escrow.nftAddress()
-      expect(result).to.be.equal(realEstate.address)
+      expect(result).to.be.equal(await realEstate.getAddress())
     })
 
     it('Returns seller', async () => {
+      const { escrow, seller } = await loadFixture(deployEscrowFixture)
+
       const result = await escrow.seller()
       expect(result).to.be.equal(seller.address)
     })
 
     it('Returns inspector', async () => {
+      const { escrow, inspector } = await loadFixture(deployEscrowFixture)
+
       const result = await escrow.inspector()
       expect(result).to.be.equal(inspector.address)
     })
 
     it('Returns lender', async () => {
+      const { escrow, lender } = await loadFixture(deployEscrowFixture)
+
       const result = await escrow.lender()
       expect(result).to.be.equal(lender.address)
     })
@@ -63,36 +116,49 @@ describe('Deployment', () => {
 
   describe('Listing', () => {
     it('Updates as listed', async () => {
+      const { escrow } = await loadFixture(listPropertyFixture)
+
       const result = await escrow.isListed(1);
       expect(result).to.be.equal(true)
     })
 
     it('Updates ownership', async () => {
-      expect(await realEstate.ownerOf(1)).to.be.equal(escrow.address)
+      const { realEstate, escrow } = await loadFixture(listPropertyFixture)
+      expect(await realEstate.ownerOf(1)).to.be.equal(await escrow.getAddress())
     })
 
     it('Returns buyer', async () => {
+      const { escrow, buyer } = await loadFixture(listPropertyFixture)
+
       const result = await escrow.buyer(1);
       expect(result).to.be.equal(buyer.address)
     })
 
     it('Returns puchase price', async () => {
+      const { escrow } = await loadFixture(listPropertyFixture)
+
       const result = await escrow.purchasePrice(1);
       expect(result).to.be.equal(tokens(10))
     })
 
     it('Returns escrow amount', async () => {
+      const { escrow } = await loadFixture(listPropertyFixture)
+
       const result = await escrow.escrowAmount(1);
       expect(result).to.be.equal(tokens(5))
     })
 
     it('should throw an error is listing is attempted by non-seller', async () => {
+      const { escrow, buyer } = await loadFixture(listPropertyFixture)
+
       await expect(escrow.connect(buyer).list(1, buyer.address, tokens(10), tokens(5))).to.be.revertedWith('Only seller can call this method');
     })
   })
 
   describe('Deposits', () => {
     it('Updates contract balance', async () => {
+      const { escrow, buyer } = await loadFixture(listPropertyFixture)
+
       const transaction = await escrow.connect(buyer).depositEarnest(1, { value: tokens(5) });
       await transaction.wait();
       const result = await escrow.getBalance()
@@ -102,6 +168,8 @@ describe('Deployment', () => {
 
   describe('Inspection ', () => {
     it('Updates inspection status', async () => {
+      const { escrow, inspector } = await loadFixture(listPropertyFixture)
+
       // Pre-condition checks
       expect(await escrow.inspectionPassed(1)).to.be.equal(false)
       // None-existing property
@@ -114,12 +182,16 @@ describe('Deployment', () => {
     })
 
     it('should throw an error if updating inspection status is attempted by non-inspector', async () => {
+      const { escrow, seller } = await loadFixture(listPropertyFixture)
+
       await expect(escrow.connect(seller).updateInspectionStatus(1, true)).to.be.revertedWith('Only inspector can call this method');
     })
   })
 
   describe('Approval ', () => {
     it('Updates approval status', async () => {
+      const { escrow, buyer, seller, lender } = await loadFixture(listPropertyFixture)
+
       // Block for the buyer to approve
       let transaction = await escrow.connect(buyer).approveSale(1);
       await transaction.wait();
@@ -143,35 +215,17 @@ describe('Deployment', () => {
       expect(await escrow.approval(1, lender.address)).to.be.equal(true);
     })
   })
-
+    
   describe('Sale ', async () => {
-    beforeEach(async () => {
-      let transaction = await escrow.connect(buyer).depositEarnest(1, { value: tokens(5) });
-      await transaction.wait();
-
-      transaction = await escrow.connect(inspector).updateInspectionStatus(1, true);
-      await transaction.wait();
-
-      transaction = await escrow.connect(buyer).approveSale(1);
-      await transaction.wait();
-
-      transaction = await escrow.connect(seller).approveSale(1);
-      await transaction.wait();
-
-      transaction = await escrow.connect(lender).approveSale(1);
-      await transaction.wait();
-
-      await lender.sendTransaction({ to: escrow.address, value: tokens(5) });    
-      
-      transaction = await escrow.connect(lender).finalizeSale(1);
-      await transaction.wait();
-    })
-
     it('Updates ownership', async () => {
+      const { realEstate, buyer } = await loadFixture(finalizeSaleFixture)
+
       expect(await realEstate.ownerOf(1)).to.be.equal(buyer.address);
     })
 
     it('Updates balance', async () => {
+      const { escrow } = await loadFixture(finalizeSaleFixture)
+
       expect(await escrow.getBalance()).to.be.equal(0);
     })
   })
